33d9c69697d7139f53d6a89e5ab8537d99a7b294
luccapsl <luccapsl211@gmail.com>
2026-03-19 20:41:55 -0300

    Implementação do código NanoID em entidade Curso + metodos print e listar

--- Entidades/Curso.java
+++ Entidades/Curso.java
@@ -1,45 +1,102 @@
+package Entidades;
+
 import java.io.*;
 import java.util.Date;
+import com.soundicly.jnanoidenhanced.jnanoid.NanoIdUtils;
 
 public class Curso {
+    protected final int TAM_CODIGO = 10;
     protected int idCurso = -1;
     protected String nome;
-    protected short codigo;
+    protected String codigo;
     protected char estado;
     protected int idUsuario;
     protected Date dataInicio;
     protected String descricao;
 
-    public Curso(String nome, short codigo, char estado, int idUsuario, Date dataInicio, String descricao) {
+    public Curso(String nome, char estado, int idUsuario, Date dataInicio, String descricao) {
         this.nome = nome;
-        this.codigo = codigo;
-        this.estado = estado;
+        this.codigo = this.genCodigo();
+        this.setEstado(estado);
         this.idUsuario = idUsuario;
         this.dataInicio = dataInicio;
         this.descricao = descricao;
         
     }
 
+    public void listar() {
+        System.out.println("("+idCurso+") "+nome+" - "+dataInicio);
+    }
+
+    public void printar() {
+        System.out.println("NOME.........: " + nome);
+        System.out.println("CODIGO.......: " + codigo);
+        System.out.println("DESCRIÇÃO....: " + descricao);
+        System.out.println("DATA INÍCIO..: " + dataInicio);
+        System.out.print("ESTADO: ");
+        
+        switch (this.estado) {
+            case ('0'):
+                System.out.println("Ativo e recebendo inscrições");
+                break;
+            case ('1'):
+                System.out.println("Ativo, mas sem novas inscrições");
+                break;
+            case ('2'):
+                System.out.println("Concluído");
+                break;
+            case ('3'):
+                System.out.println("Coancelado");
+                break;
+            default:
+                System.out.println("[ERRO] - Estado de curso invalido");
+                throw new AssertionError();
+        }
+        
+    }
+
+    /**
+     * GEra o codigo NanoId de TAM_CODIGO caracteres
+     *
+     * @return String
+     */
+    protected String genCodigo() {
+        return NanoIdUtils.randomNanoId(TAM_CODIGO);
+    }
+
+    /**
+     * Converte o array de bytes enviado para atributos do Objeto
+     *
+     * @param byte[] ba contendo os dados do objeto em bytes
+     * @return void
+     * @throws IOException se ocorrer erro ao ler os dados do array
+     */
     public void fromByteArray(byte[] ba) throws IOException {
         ByteArrayInputStream bais = new ByteArrayInputStream(ba);
         DataInputStream dis = new DataInputStream(bais);
 
         this.idCurso = dis.readInt();
         this.nome = dis.readUTF();
-        this.codigo = dis.readShort();
+        this.codigo = dis.readUTF();
         this.estado = dis.readChar();
         this.idUsuario = dis.readInt();
         this.dataInicio = new Date(dis.readLong());
         this.descricao = dis.readUTF();
     }
 
+    /**
+     * Converte os atributos do Objeto para um array de bytes
+     *
+     * @return byte[] contendo os dados do objeto em bytes
+     * @throws IOException se ocorrer erro ao escrever os dados no array
+     */
     public byte[] toByteArray() throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(baos);
 
         dos.writeInt(this.idCurso);
         dos.writeUTF(this.nome);
-        dos.writeShort(this.codigo);
+        dos.writeUTF(this.codigo);
         dos.writeChar(this.estado);
         dos.writeInt(this.idUsuario);
         dos.writeLong(this.dataInicio.getTime());
@@ -48,11 +105,11 @@ public class Curso {
         return baos.toByteArray();
     }
 
-    public short getCodigo() {
+    public String getCodigo() {
         return codigo;
     }
 
-    public void setCodigo(short codigo) {
+    public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
 
@@ -77,7 +134,11 @@ public class Curso {
     }
 
     public void setEstado(char estado) {
-        this.estado = estado;
+        if (estado < 0 || estado > 3) {
+            System.out.println("[ERRO] - Estado invalido");
+        } else {
+            this.estado = estado;
+        }
     }
 
     public int getIdCurso() {
